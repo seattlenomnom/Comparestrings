7ebd5edfcf102eeb2ad3feaa71f8b5214cee99e1
seattlenomnom <mark.crapser@gmail.com>
2022-07-19 12:52:55 -0700

    filled in equalstrings and main

--- comparestrings.c
+++ comparestrings.c
@@ -16,18 +16,13 @@ bool equalstrings(const char s1[], const char s2[]);
 int main(int argc, char *argv[]) {
 
 
+    const char s1[] = "string one is awesome!";
+    const char s2[] = "string one is awesome!";
 
-
-
-
-
-
-
-
-
-
-
-
+    if(equalstrings(s1, s2))
+        printf("The strings are equal.\n");
+    else
+        printf("The strings are unequal.\n");
 
 
     return(0);
@@ -38,19 +33,17 @@ int main(int argc, char *argv[]) {
 
 bool equalstrings(const char s1[], const char s2[]) {
 
-
+    int i;
     bool areEqual;
 
+    i = 0;
+    while((s1[i] == s2[i]) && (s1[i] != '\0') && (s2[i] != '\0'))
+        ++i;
 
-
-
-
-
-
-
-
-
-
+    if(s1[i] == '\0' && s2[i] == '\0')
+        areEqual = true;
+    else
+        areEqual = false;
 
     return(areEqual);
 }
